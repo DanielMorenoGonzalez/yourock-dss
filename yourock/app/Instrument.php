e574d492ee8707cd5c5952e330715adca835adea
Daniel Moreno <dmg66@alu.ua.es>
2017-06-18 19:40:47 +0200

    Añadidos métodos get a la clase Instrument

--- yourock/app/Instrument.php
+++ yourock/app/Instrument.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Category;
+use App\Orderline;
 
 class Instrument extends Model
 {
@@ -17,4 +19,28 @@ class Instrument extends Model
         return $this->hasMany('App\Orderline');
     }
 
+    //Función que devuelve el nombre de un instrumento
+    public function getName() {
+        return $this->name;
+    }
+
+    //Función que devuelve la descripción de un instrumento
+    public function getDescription() {
+        return $this->description;
+    }
+
+    //Función que devuelve el stock de un instrumento
+    public function getStock() {
+        return $this->stock;
+    }
+
+    //Función que devuelve el precio de un instrumento
+    public function getPrice() {
+        return $this->price;
+    }
+
+    //Función que devuelve la url de la foto de un instrumento
+    public function getUrlPhoto() {
+        return $this->urlPhoto;
+    }
 }
